755489d7d2c3ef5ee376ae03a6efe406358db37e
JustinHair <justin.hair@epicgames.com>
2014-07-16 16:16:42 -0400

    Initial checkin of BleacherBaseball project

---
+++ BleacherBaseball.cpp
@@ -0,0 +1,37 @@
+// Baseball simulation game
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int FPitcherRandomAttributesGenerator();
+int FBatterRandomAttributesGenerator();
+
+
+void main()
+{
+	//Choose a pitcher
+	//Choose a batter
+	
+	//Pitcher chooses what pitch to throw
+	//Pitcher chooses what location
+
+
+
+	//Batter chances on recognizing the pitch
+	//Batter determines pitch location
+
+
+	//Batter decides to swing or take
+
+
+	//Actual pitch location vs precieved by batter
+	//calculates hit or whiff
+	
+
+
+
+
+}
---
+++ Pitcher.h
@@ -0,0 +1,13 @@
+#pragma once
+
+{
+public:
+
+
+
+private:
+
+
+
+
+};
