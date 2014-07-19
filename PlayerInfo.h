4fc2db285423c2a0739c64397b5faa78878f82eb
JustinHair <justin.hair@epicgames.com>
2014-07-19 19:02:07 -0400

    Adding all player info (position player and pitcher) to one class

--- PlayerInfo.h
+++ PlayerInfo.h
@@ -4,20 +4,27 @@
 
 using namespace std;
 
-//Batter Class
-class PositionPlayer
+//Baseball player Class
+class Player
 {
 public:
-	PositionPlayer();
+	Player();
 
+	 
 
 	//PositionPlayer Deconstructor
-	~PositionPlayer();
+	~Player();
 
 private:
 	//Player Names and Team Name
 	string PlayerName;
 	string TeamName;
+	string Throws;				// Left or Right
+	string BatHand;				// Left, Right, or Switch
+
+	//Position
+	string Position;
+	int PositinID;				// 1 - 9 + 0 for DH
 
 	//The players age.
 	int PlayerAge;
@@ -27,6 +34,7 @@ private:
 	int Timing;					// Determines if the player hits the as well as to what part of the field, and fair or foul.
 	int Contact;				// How well does the player make contact
 	int Power;					// Possibly rename to "Drive"
+
 	int BuntingSkill;			// How well the player bunts.
 
 	int TendencyGroundball;		// How likely is the player going to hit a groundball, linedrive, or a flyball.  
@@ -42,12 +50,14 @@ private:
 	int Range;
 	int GloveWork;
 
+
+
 };
 
-PositionPlayer::PositionPlayer()
+Player::Player()
 {
 }
 
-PositionPlayer::~PositionPlayer()
+Player::~Player()
 {
 }
