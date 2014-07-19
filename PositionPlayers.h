cf525c22f859dfdec978b0bd8654264321a734ae
JustinHair <justin.hair@epicgames.com>
2014-07-19 14:41:18 -0400

    Updating the Position Player header file.

--- PositionPlayers.h
+++ PositionPlayers.h
@@ -19,12 +19,19 @@ private:
 	string PlayerName;
 	string TeamName;
 
+	//The players age.
+	int PlayerAge;
+
 	//Player hitting attributes
 	int BattersEye;				// How easily the batter can determine the pitch type and location.
-	int Timing;					//
-	int Contact;
-	int Power;
-	int BuntingSkill;
+	int Timing;					// Determines if the player hits the as well as to what part of the field, and fair or foul.
+	int Contact;				// How well does the player make contact
+	int Power;					// Possibly rename to "Drive"
+	int BuntingSkill;			// How well the player bunts.
+
+	int TendencyGroundball;		// How likely is the player going to hit a groundball, linedrive, or a flyball.  
+	int TendencyFlyball;		// How likely is the player going to hit a groundball, linedrive, or a flyball.  
+	int TendencyLinedrive;		// How likely is the player going to hit a groundball, linedrive, or a flyball.  
 
 	//Speed - Used for running the bases and fielding.
 	int Speed;
@@ -32,7 +39,7 @@ private:
 	//Fielding
 	int ArmStrenght;
 	int ArmAccuracy;
-	int FieldRange;
+	int Range;
 	int GloveWork;
 
 };
